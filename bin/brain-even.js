3da5a822656e8b8c0b0b97ccb1e84c9b9bea4f46
Игорь Ротенбергер <rotenberger.igor@hexly.ru>
2023-02-18 22:02:28 +0500

    fix even-game logic, was trouble with steps count

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -3,17 +3,19 @@ import readlineSync from 'readline-sync';
 const isEvenTextRule = 'Answer "yes" if the number is even, otherwise answer "no".';
 
 const gameRules = (theCorrrectAnswer, enteredResponse, userName, roundPosition) => {
-  let result = 'true';
-  if (theCorrrectAnswer === enteredResponse) {
-    console.log('Correct!');
-  } else {
-    result = 'false';
-    console.log(`'${enteredResponse}' is wrong answer ;(. Correct answer was '${theCorrrectAnswer}'. Let's try again, ${userName}!`);
-  }
+  let result = true;
   if (roundPosition >= 3) {
     console.log(`Congratulations, ${userName}!`);
+    return result;
   }
-  return result;
+  if (theCorrrectAnswer === enteredResponse) {
+    console.log('Correct!');
+    return result;
+  } else {
+    result = false;
+    console.log(`'${enteredResponse}' is wrong answer ;(. Correct answer was '${theCorrrectAnswer}'. Let's try again, ${userName}!`);
+    return result;
+  }
 }
 
 const randomNum = (min, max) => {
@@ -25,20 +27,19 @@ const evenGame = () => {
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
   console.log(isEvenTextRule);
-  for (let roundPosition = 0; roundPosition <= 3; roundPosition += 1) {
+  for (let roundPosition = 1; roundPosition <= 3; roundPosition += 1) {
     const questionNum = randomNum(1, 100);
     const isEven = (num) => num % 2 === 0;
     const theCorrrectAnswer = isEven(questionNum) ? 'yes' : 'no';
     const enteredResponse = readlineSync.question(`Question: ${questionNum} \n `);
     const result = gameRules(theCorrrectAnswer, enteredResponse, userName, roundPosition);
-    if (result === 'false') {
+    if (result === false) {
       break;
     }
   }
 }
 
 evenGame();
-gameRules();
 
 
 
