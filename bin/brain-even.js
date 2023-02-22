2b20102fae3b6e606f5a7c4bfe50ff064498fd0c
Игорь Ротенбергер <rotenberger.igor@hexly.ru>
2023-02-22 12:24:25 +0500

    restruction of file-tree, to add import-export dependencies

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,46 +1,4 @@
 #!/usr/bin/env node
-import readlineSync from 'readline-sync';
+import { evenGame } from '../src/games/even-logic.js';
 
-const isEvenTextRule = 'Answer "yes" if the number is even, otherwise answer "no".';
-
-const gameRules = (theCorrrectAnswer, enteredResponse, userName, roundPosition) => {
-  let result = true;
-  if (roundPosition >= 3) {
-    console.log('Correct!');
-    console.log(`Congratulations, ${userName}!`);
-    return result;
-  }
-  if (theCorrrectAnswer === enteredResponse) {
-    console.log('Correct!');
-    return result;
-  } else {
-    result = false;
-    console.log(`'${enteredResponse}' is wrong answer ;(. Correct answer was '${theCorrrectAnswer}'. Let's try again, ${userName}!`);
-    return result;
-  }
-}
-
-const randomNum = (min, max) => {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-}
-
-const evenGame = () => {
-  console.log('Welcome to the Brain Games!');
-  const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${userName}!`);
-  console.log(isEvenTextRule);
-  for (let roundPosition = 1; roundPosition <= 3; roundPosition += 1) {
-    const questionNum = randomNum(1, 100);
-    const isEven = (num) => num % 2 === 0;
-    const theCorrrectAnswer = isEven(questionNum) ? 'yes' : 'no';
-    const enteredResponse = readlineSync.question(`Question: ${questionNum} \n `);
-    const result = gameRules(theCorrrectAnswer, enteredResponse, userName, roundPosition);
-    if (result === false) {
-      break;
-    }
-  }
-}
-
-evenGame();
-
-
+calcGame();
--- src/index.js
+++ src/index.js
@@ -1,7 +1,24 @@
 #!/usr/bin/env node
 import readlineSync from 'readline-sync';
 
-const gameInit = (theCorrrectAnswer, enteredResponse, userName, roundPosition) => {
+export const randomNum = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  };
+
+
+export const calculator = (operand, randomNum1, randomNum2) => {
+    const operands = ['+', '-', '*'];
+    if (operand === operands[1]) {
+      return String(randomNum1 - randomNum2);
+    }
+    if (operand === operands[2]) {
+      return String(randomNum1 * randomNum2);
+    }
+    return String(randomNum1 + randomNum2);
+  };
+
+
+export const gameInit = (theCorrrectAnswer, enteredResponse, userName, roundPosition) => {
     let result = true;
     if (roundPosition >= 3) {
       console.log('Correct!');
@@ -17,4 +34,3 @@ const gameInit = (theCorrrectAnswer, enteredResponse, userName, roundPosition) =
       return result;
     }
   }
-export default gameInit;
