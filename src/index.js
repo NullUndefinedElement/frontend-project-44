883ee7d47a58aac297388cc461243add05ff2364
Игорь Ротенбергер <rotenberger.igor@hexly.ru>
2023-02-22 13:34:58 +0500

    end calc-game and tree restruction, troubleshooting before asciinema recording

--- src/index.js
+++ src/index.js
@@ -2,35 +2,35 @@
 import readlineSync from 'readline-sync';
 
 export const randomNum = (min, max) => {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-  };
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
 
 
 export const calculator = (operand, randomNum1, randomNum2) => {
-    const operands = ['+', '-', '*'];
-    if (operand === operands[1]) {
-      return String(randomNum1 - randomNum2);
-    }
-    if (operand === operands[2]) {
-      return String(randomNum1 * randomNum2);
-    }
-    return String(randomNum1 + randomNum2);
-  };
+  const operands = ['+', '-', '*'];
+  if (operand === operands[1]) {
+    return String(randomNum1 - randomNum2);
+  }
+  if (operand === operands[2]) {
+    return String(randomNum1 * randomNum2);
+  }
+  return String(randomNum1 + randomNum2);
+};
 
 
 export const gameInit = (theCorrrectAnswer, enteredResponse, userName, roundPosition) => {
-    let result = true;
-    if (roundPosition >= 3) {
-      console.log('Correct!');
-      console.log(`Congratulations, ${userName}!`);
-      return result;
-    }
-    if (theCorrrectAnswer === enteredResponse) {
-      console.log('Correct!');
-      return result;
-    } else {
-      result = false;
-      console.log(`'${enteredResponse}' is wrong answer ;(. Correct answer was '${theCorrrectAnswer}'. Let's try again, ${userName}!`);
-      return result;
-    }
+  let result = true;
+  if (roundPosition >= 3) {
+    console.log('Correct!');
+    console.log(`Congratulations, ${userName}!`);
+    return result;
   }
+  if (theCorrrectAnswer === enteredResponse) {
+    console.log('Correct!');
+    return result;
+  } else {
+    result = false;
+    console.log(`'${enteredResponse}' is wrong answer ;(. Correct answer was '${theCorrrectAnswer}'. Let's try again, ${userName}!`);
+    return result;
+  }
+}
